20650f3c74a777fc94904afee3f53967015dfbdd
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-22 16:41:43 +0530

    Added: Main is modofied to take mood message in constructor

---
+++ src/main/java/com/bridgelabz/MoodAnalyser/moodAnalyser.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.MoodAnalyser;
+
+public class moodAnalyser {
+    private String message;
+    
+    public moodAnalyser(){
+    }
+    
+    public moodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to mood analyser Program");
+    }
+    
+    public String analyseMood(String message) {
+        this.message = message;
+        return analyseMood();
+    }
+    
+    public String analyseMood() {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}
